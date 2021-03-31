25a3229216b6097a352a1486bc2e91c607c64a70
Kalovelo <apostoloskalovelonis@gmail.com>
2021-03-31 21:53:35 +0300

    feat(wie): implement new design

--- src/components/Wie.vue
+++ src/components/Wie.vue
@@ -1,153 +1,122 @@
 <template>
   <section class="section wie" id="wie">
     <h2 class="title">Women in Engineering</h2>
-    <div class="full-width">
-      <div class="wie__image"></div>
-      <div class="quote">
+    <div class="wie__wrapper">
+      <div class="wie__quote">
         <h3 class="quote__content">
-          “I believe in the unlimited power of women in the context of science
-          and engineering”
+          “I believe in the unlimited power of women in the context of science and engineering”
         </h3>
+        <h3 class="quote__author">-Elizabeth Holmes</h3>
       </div>
-      <h3 class="quote__author">-Elizabeth Holmes</h3>
+      <p class="wie__description">
+        {{ $t("wieText") }}
+      </p>
+      <a href="" class="wie__cta btn button--outlined ">
+        <font-awesome :icon="['fab', 'facebook-f']" size="1x" />
+        <span> WiE Page</span>
+      </a>
+      <img :src="image" class="wie__image" />
     </div>
-    <p class="description">
-      {{ $t("wieText") }}
-    </p>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: require("@/assets/img/wie.png"),
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import "~/assets/scss/variables";
 
 .wie {
-  height: 600px;
+  &__wrapper {
+    gap: 50px;
+    display: grid;
+    grid-template-areas:
+      "img"
+      "quote"
+      "description"
+      "cta";
+  }
 
-  @media (max-width: $screen-md-min) {
-    height: 550px;
+  &__quote {
+    grid-area: quote;
+
+    h3 {
+      font-size: 1.3rem;
+    }
+  }
+
+  &__description {
+    grid-area: description;
+    font-size: 1.1rem;
+  }
+
+  &__cta {
+    grid-area: cta;
+    width: max-content;
   }
 
   &__image {
-    position: absolute;
-    right: 0;
-    z-index: -99;
-    width: 900px;
-    height: 400px;
-    background-repeat: no-repeat;
-    background-image: linear-gradient(
-        90deg,
-        rgba(255, 255, 255, 0.57) 6.72%,
-        rgba(255, 255, 255, 0) 50%
-      ),
-      url("../assets/img/wie.jpeg");
+    grid-area: img;
+    width: 80% !important;
+  }
 
-    @media (max-width: $screen-md-min) {
-      right: -55px;
-      background-repeat: no-repeat;
-      background-image: linear-gradient(
-          90deg,
-          rgba(255, 255, 255, 0.87) 6.72%,
-          rgba(255, 255, 255, 0) 50%
-        ),
-        url("../assets/img/wie-sm.jpeg");
-      width: 200px;
-      height: 200px;
-      object-fit: cover;
-      background-size: cover;
-      border-radius: 150px;
+  @media (min-width: $screen-md-min) {
+    &__wrapper {
+      grid-template-areas:
+        ". img"
+        "quote img"
+        "description img"
+        "cta img";
+      gap: 10px;
     }
 
-    @media (min-width: $screen-sm-min) and (max-width: $screen-lg-min) {
-      right: 5px;
-      background-repeat: no-repeat;
-      background-image: linear-gradient(
-          90deg,
-          rgba(255, 255, 255, 0.7) 6.72%,
-          rgba(255, 255, 255, 0) 50%
-        ),
-        url("../assets/img/wie-sm.jpeg");
-      width: 300px;
-      height: 300px;
-      object-fit: cover;
-      background-size: cover;
-      border-radius: 0 !important;
+    &__description {
+      font-size: 0.9rem;
     }
-  }
-}
 
-.quote {
-  padding: 50px 0;
-  max-width: 60%;
+    &__quote h3 {
+      font-size: 1rem;
+    }
 
-  @media (max-width: $screen-sm-min) {
-    margin-top: 20px;
-    font-size: 10px !important;
-    max-width: 55%;
-    padding: 0;
-  }
-
-  @media (min-width: $screen-sm-min) and (max-width: $screen-md-min) {
-    margin-top: 55px;
-    font-size: 10px !important;
-    max-width: 55%;
-    padding: 0;
-  }
-
-  &__content {
-    @media (max-width: $screen-sm-min) {
-      margin-top: 15px;
-      font-size: 2em !important;
+    &__image {
+      width: 100% !important;
     }
   }
 
-  &__author {
-    margin-top: -20px;
-
-    @media (max-width: $screen-sm-min) {
-      font-size: 1.4rem;
-      margin-top: 30px;
+  @media (min-width: $screen-lg-min) {
+    &__wrapper {
+      gap: 20px;
+      grid-template-rows: 50px 1fr 1fr 40px;
+      grid-template-areas:
+        ". img"
+        "quote img"
+        "description img"
+        "cta img";
     }
 
-    @media (min-width: $screen-sm-min) and (max-width: $screen-md-min) {
-      margin-top: 30px;
+    &__description {
+      font-size: 1rem;
+    }
+
+    &__quote h3 {
+      font-size: 1.3rem;
     }
   }
-}
 
-.description {
-  max-width: 60%;
-  margin-top: 80px;
-  font-size: 20px;
+  @media (min-width: $screen-xl-min) {
+    &__description {
+      font-size: 1.5rem;
+    }
 
-  @media (max-width: $screen-sm-min) {
-    margin-top: 20px;
-    max-width: 100%;
-    font-size: 18px;
-    padding: 0 6px;
-  }
-
-  @media (min-width: $screen-sm-min) and (max-width: $screen-md-min) {
-    margin-top: 55px;
-    max-width: 100%;
-    font-size: 20px;
-    padding: 0 8px;
-  }
-}
-
-.full-width {
-  @media (max-width: $screen-md-min) {
-    width: 100vw;
-    position: relative;
-    left: 50%;
-    right: 50%;
-    margin-left: -50vw;
-    margin-right: -50vw;
-    overflow: hidden;
-    height: 250px;
-    padding-left: 20px !important;
-
-    @media (min-width: $screen-sm-min) and (max-width: $screen-md-min) {
-      padding-left: 40px !important;
+    &__quote h3 {
+      font-size: 1.75rem;
     }
   }
 }
